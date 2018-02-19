b06b6569ef10ff3b718e4757861d0d94ddb7591c
Christine Andrianopoulou <christineandrian@hotmail.com>
2018-02-19 21:12:14 +0200

    Added askisi 8 via upload

--- askisi8.py
+++ askisi8.py
@@ -9,18 +9,29 @@ Created on Sun Feb 18 18:06:33 2018
 import numpy as np
 
 x=np.random.random_integers(-30,30, size=30)
+
+print("Here's a list with 30 random integers in the span (-30,30): ")
 print(x)
 flag=1
-     
-y=np.unique(x)
+comb=np.zeros((200,3))
+c=0
+x=np.sort(x)
 
-for i in range(0,(len(y)-2),1):
-    for j in range(1,(len(y)-1),1):
-        num=(y[i]+y[j])
-        for k in range(2,len(y),1):
-            if y[k]==-num:
-                print(y[i],y[j],y[k])
+for i in range(0,(len(x)-2),1):
+    for j in range(1,(len(x)-1),1):
+        for k in range(2,len(x),1):
+            if x[i]+x[j]+x[k]==0 and x[i]<=x[j]<=x[k]:
+                comb[c,:]=np.hstack(([x[i],x[j],x[k]]))
+                c+=1
                 flag=0
 
+print("Combos found: ")
+
+for i in range(0,c+1,1):
+    if comb[i+1,1]<>comb[i,1]:       
+        print(comb[i,:])
+
+
 if flag==1:
-    print("Δεν υπάρχει συνδυασμός")
+    print("No combo available")
+
