9764eaf60c49dc45667d9c5772f48242d8f514d6
Christine Andrianopoulou <christineandrian@hotmail.com>
2018-02-19 17:40:46 +0200

    Added askisi 8

--- askisi8.py
+++ askisi8.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb 11 17:51:04 2018
+Created on Sun Feb 18 18:06:33 2018
 
 @author: Bravebirdie
 """
-
 #άσκηση 8 30 random +ve and -ve numbers + (3άδες = 0)
 
 import numpy as np
 
 x=np.random.random_integers(-30,30, size=30)
-num=0
 print(x)
 flag=1
-for i,j in zip(range(0,27),range(1,28)):
-    num= (x[i]+x[j])
-    if -30<=num<=30:
-        for k in range(j+1,29,1):
-            if x[k]==-num:
-                print(x[i],x[j],x[k])
+     
+y=np.unique(x)
+
+for i in range(0,(len(y)-2),1):
+    for j in range(1,(len(y)-1),1):
+        num=(y[i]+y[j])
+        for k in range(2,len(y),1):
+            if y[k]==-num:
+                print(y[i],y[j],y[k])
                 flag=0
+
 if flag==1:
     print("Δεν υπάρχει συνδυασμός")
-
